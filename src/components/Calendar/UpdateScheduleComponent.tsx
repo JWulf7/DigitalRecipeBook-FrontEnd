10b5ca83556709e38bba2a1c52c55233df64b3be
Jonathan Wulf <j.wulf@live.com>
2024-04-02 15:40:55 -0500

    begin updateScheduleComponent

--- src/components/Calendar/UpdateScheduleComponent.tsx
+++ src/components/Calendar/UpdateScheduleComponent.tsx
@@ -1,6 +1,42 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
-const UpdateScheduleComponent = () => {
+interface Props {
+  date: string;
+  schedule: Schedule;
+}
+
+interface Schedule {
+  date: string;
+  todaysPlannedMeals: PlannedMeal[];
+  tomorrowsPlannedMeals: PlannedMeal[];
+  prepList: string[];
+  allMealsPlanned: any;
+  notes: string[];
+  actualMeals: any;
+}
+
+let meals : string[] = ["BREAKFAST", "BRUNCH", "LUNCH", "SNACK", "DINNER", "DESSERT", "ANY"];
+
+interface PlannedMeal {
+  id: number;
+  name: string;
+  meal: string;
+  menu: RecipeName[];
+  allDatesCooked: Date[];
+  notes: string[];
+}
+
+interface RecipeName {
+  id: number;
+  name: string;
+}
+
+// const UpdateScheduleComponent = ({date, schedule} : Props) => {
+  const UpdateScheduleComponent = () => {
+
+    const { date } = useParams()
+
   return (
     <div>UpdateScheduleComponent</div>
   )
